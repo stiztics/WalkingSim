d629e4ced857a33c704bec2bbdc603b6836513b9
Justin Li <107675909+Justinyutingli@users.noreply.github.com>
2022-09-29 14:04:29 -0400

    Add files via upload

---
+++ Assets/Code/Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    int speed = 25;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed*Time.deltaTime, 0, Space.World) ;      
+    }
+}
