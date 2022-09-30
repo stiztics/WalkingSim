d6801a44b8441f900c050dcdcd58738140340001
stiztics <il2149@nyu.edu>
2022-09-29 21:17:11 -0400

    Replaced zombie prefab

---
+++ Assets/Code/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform target;
+    private Rigidbody rb;
+    public float moveSpeed = 5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+        pos.y = 2.92f;
+        rb.MovePosition(pos);
+        transform.LookAt(target);
+    }
+}
